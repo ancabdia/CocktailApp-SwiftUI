038460b1ddda463280f177dd73a5187f090e5146
ancadbia <ancabdia.contact@gmail.com>
2023-08-06 20:21:18 +0200

    Initial Commit

---
+++ CocktailAPP/CocktailAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAPPApp.swift
+//  CocktailAPP
+//
+//  Created by Cabrera Diaz Andrés on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
