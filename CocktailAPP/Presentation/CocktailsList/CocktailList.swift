c937ae1e0502198c607f38fe5c44982af0480d07
ancadbia <ancabdia.contact@gmail.com>
2023-08-07 14:36:54 +0200

    Search implemented over cocktails by name

--- CocktailAPP/Presentation/CocktailsList/CocktailList.swift
+++ CocktailAPP/Presentation/CocktailsList/CocktailList.swift
@@ -10,14 +10,22 @@ import SwiftUI
 struct CocktailList: View {
     @EnvironmentObject var rootViewModel: RootViewModel
     @ObservedObject var cocktailViewModel: CocktailListViewModel
+    @State private var searchCocktail: String = ""
     
     init(cocktailViewModel: CocktailListViewModel) {
         self.cocktailViewModel = cocktailViewModel
     }
     
+    var filteredCocktails: [Cocktail] {
+        guard !searchCocktail.isEmpty else { return cocktailViewModel.cocktails }
+        //return new call to API with searchCocktail term as name
+        cocktailViewModel.researchByName(cocktailName: searchCocktail)
+        return cocktailViewModel.cocktails.filter{$0.name.localizedCaseInsensitiveContains(searchCocktail)}
+    }
+    
     var body: some View {
-        NavigationView {
-            List(cocktailViewModel.cocktails) { cocktail in
+        NavigationStack {
+            List(filteredCocktails) { cocktail in
                 NavigationLink{
                     CocktailDetailView(cocktail: cocktail)
                 } label: {
@@ -27,6 +35,7 @@ struct CocktailList: View {
             .navigationTitle("Cocktails")
             .navigationBarTitleDisplayMode(.inline)
             .scrollContentBackground(.hidden)
+            .searchable(text: $searchCocktail, placement: .automatic, prompt: "Seach cocktails")
         }
     }
 }
--- CocktailAPP/Presentation/CocktailsList/CocktailListViewModel.swift
+++ CocktailAPP/Presentation/CocktailsList/CocktailListViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 final class CocktailListViewModel: ObservableObject {
     // MARK: Properties
     private let repository: RepositoryProtocol
@@ -15,9 +16,24 @@ final class CocktailListViewModel: ObservableObject {
         self.repository = repository
         DispatchQueue.main.async {
             Task {
-                guard let cocktailsRemote = try? await repository.filterByName(cocktailName: "martini") else { //TODO: - Change this to a search field
+                //By default search bar returns 10 first from API
+                guard let cocktailsRemote = try? await repository.filterByName(cocktailName: "") else {
                     self.cocktails = []
                     print("Unable to get cocktails from api")
+                    return
+                }
+                self.cocktails = cocktailsRemote
+            }
+        }
+    }
+    
+    //Function to return the searched cocktails by name
+    func researchByName(cocktailName: String) {
+        DispatchQueue.main.async {
+            Task {
+                guard let cocktailsRemote = try? await self.repository.filterByName(cocktailName: cocktailName) else {
+                    self.cocktails = []
+                    print("Unable to get cocktails from api on researchByName")
                     return
                 }
                 self.cocktails = cocktailsRemote
